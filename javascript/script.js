f49b5a5ac931963254811f9dd5c6501542f4f90b
Renan Lucena dos Santos <69248786+RenanLucenaDeveloper@users.noreply.github.com>
2021-08-08 20:47:36 -0300

    inicio do js

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+function initClickOutside() {
+    
+}
+
+function initMenuMobile() {
+    const botaoAbrir = document.querySelector('[data-menuMobile="abrir"]');
+    const botaoFechar = document.querySelector('[data-menuMobile="fechar"]');
+    const menuMobile = document.querySelector('[data-menuMobile="menu"]');
+    const linkMenu = document.querySelectorAll('[data-menuMobile="linkMenu"]');
+    const eventos = ['click', 'touchstart'];
+
+    function abrirMenu() {
+        menuMobile.classList.add('ativo');
+        botaoAbrir.classList.add('ativo');
+    }
+
+    function fecharMenu() {
+        menuMobile.classList.remove('ativo');
+        botaoAbrir.classList.remove('ativo');
+    }
+
+    //eventos.forEach((e) => {botaoAbrir.addEventListener(e, abrirMenu)});
+    botaoAbrir.addEventListener('touchstart', abrirMenu);
+    botaoFechar.addEventListener('touchstart', fecharMenu);
+    linkMenu.forEach((i) => { i.addEventListener('touchstart', fecharMenu) }
+)
+}
+initMenuMobile();
+
+function initFiltrar() {
+    const botaoAbrir = document.querySelector('[data-navMobile="abrir"]');
+    const menuFiltrar = document.querySelector('[data-navMobile="menuFiltrar"]');
+    
+    function ativaFiltrar() {
+        menuFiltrar.classList.toggle('ativo');
+    }
+
+    botaoAbrir.addEventListener('touchstart', ativaFiltrar);
+}
+
+initFiltrar()
