583830ed8dea23aa53bf90a54a83b7e38a819e63
Renan Lucena dos Santos <69248786+RenanLucenaDeveloper@users.noreply.github.com>
2021-08-09 20:50:18 -0300

    v6

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,20 @@
-function initClickOutside() {
-    
+function initHeaderMobile() {
+    const header = document.querySelector('[data-header]');
+    let lastScrollTop = 0;
+
+    function toggleClasse() {
+        var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+        if (st > lastScrollTop){
+            header.classList.add('escondido');
+        } else {
+            header.classList.remove('escondido');
+        }
+        lastScrollTop = st <= 0 ? 0 : st;
+    }
+
+    window.addEventListener('scroll', toggleClasse, false);
 }
+initHeaderMobile()
 
 function initMenuMobile() {
     const botaoAbrir = document.querySelector('[data-menuMobile="abrir"]');
@@ -26,6 +40,10 @@ function initMenuMobile() {
 }
 initMenuMobile();
 
+function initClickOutside() {
+    
+}
+
 function initFiltrar() {
     const botaoAbrir = document.querySelector('[data-navMobile="abrir"]');
     const menuFiltrar = document.querySelector('[data-navMobile="menuFiltrar"]');
@@ -37,5 +55,8 @@ function initFiltrar() {
 
     botaoAbrir.addEventListener('touchstart', ativaFiltrar);
 }
-
 initFiltrar()
+
+function initModalProduto() {
+    
+}
