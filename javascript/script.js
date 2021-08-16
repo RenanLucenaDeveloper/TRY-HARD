eba9be7485cda74bf6e6136b84540ea4358fe0af
Renan Lucena dos Santos <69248786+RenanLucenaDeveloper@users.noreply.github.com>
2021-08-16 19:26:23 -0300

    mobile v3

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ function initHeaderMobile() {
 
     function toggleClasse() {
         var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-        if (st > lastScrollTop){
+        if (st > lastScrollTop) {
             header.classList.add('escondido');
         } else {
             header.classList.remove('escondido');
@@ -24,67 +24,81 @@ function initMenuMobile() {
     const menuMobile = document.querySelector('[data-menuMobile="menu"]');
     const linkMenu = document.querySelectorAll('[data-menuMobile="linkMenu"]');
 
+    //Guarda o document.documentElement  e o atributo data-outside em uma constante
+    const html = document.documentElement;
+    const outside = 'data-outside'
+
     function abrirMenu(e) {
         menuMobile.classList.add('ativo');
         botaoAbrir.classList.add('ativo');
-        initClickOutside(menuMobile, eventos, () => {
-            menuMobile.classList.remove('ativo');
-            botaoAbrir.classList.remove('ativo');
-        });
+        initClickOutside(menuMobile, eventos, fecharMenu);
     }
 
     function fecharMenu() {
         menuMobile.classList.remove('ativo');
         botaoAbrir.classList.remove('ativo');
-    }
-        
+
+        //Remove os event listeners e o atributo
+        events.forEach((e) => {
+            html.removeEventListener(e, removeClasse);
+        })
+        html.removeAttribute(outside);
+    };
+
     function initClickOutside(element, events, callback) {
 
-        //Guarda o document.documentElement  e o atributo data-outside em uma constante
-        const html = document.documentElement;
-        const outside = 'data-outside'
-    
         //Verifica se o elemento não tem o atributo data-outside para evitar de adicionar muitos event listeners no html
-        if(!element.hasAttribute(outside)){
-    
+        if (!html.hasAttribute(outside)) {
+
             //Adiciona os event listeners no html e logo adiciona o atributo data-outside
             events.forEach((e) => {
-                setTimeout(() => {html.addEventListener(e, removeClasse)}, 0);
+                setTimeout(() => { html.addEventListener(e, removeClasse) }, 0);
             })
-            element.setAttribute(outside, '');
+            html.setAttribute(outside, '');
         }
-    
+
+        function fecharMenu() {
+            menuMobile.classList.remove('ativo');
+            botaoAbrir.classList.remove('ativo');
+
+            //Remove os event listeners e o atributo
+            events.forEach((e) => {
+                html.removeEventListener(e, removeClasse);
+            })
+            html.removeAttribute(outside);
+        };
+
         //Função que remove a classe ativo usando a callback que o ativador desta função passar
         function removeClasse(event) {
-    
+
             //Verifica se o click foi fora do elemento
-            if(!element.contains(event.target)) {
-    
+            if (!element.contains(event.target)) {
+
                 //Remove os event listeners e o atributo
                 events.forEach((e) => {
                     html.removeEventListener(e, removeClasse);
                 })
-                element.removeAttribute(outside);
-    
-                //Ativa a callback passada pelo parâmetro (callback)
-                callback();
-                console.log('ativou a função');
+                html.removeAttribute(outside);
 
+                //Ativa a callback passada pelo parâmetro (callback)
+                fecharMenu();
+                console.log('ativou o outside');
             }
         }
+
+        botaoFechar.addEventListener('click', fecharMenu);
+        linkMenu.forEach((i) => { i.addEventListener('click', fecharMenu) }
+        )
     }
 
     botaoAbrir.addEventListener('click', abrirMenu);
-    botaoFechar.addEventListener('click', fecharMenu);
-    linkMenu.forEach((i) => { i.addEventListener('click', fecharMenu) }
-)
 }
 initMenuMobile();
 
 function initFiltrar() {
     const botaoAbrir = document.querySelector('[data-navMobile="abrir"]');
     const menuFiltrar = document.querySelector('[data-navMobile="menuFiltrar"]');
-    
+
     function ativaFiltrar() {
         menuFiltrar.classList.toggle('ativo');
         botaoAbrir.classList.toggle('ativo');
@@ -95,7 +109,7 @@ function initFiltrar() {
 initFiltrar()
 
 function initModalProduto() {
-    const imagensDosProdutos = document.querySelectorAll('[data-modalProduto="imagem"]');    
+    const imagensDosProdutos = document.querySelectorAll('[data-modalProduto="imagem"]');
     const botaoDosProdutos = document.querySelectorAll('[data-modalProduto="botao"]');
     const botaoFecharModal = document.querySelector('[data-modalProduto="fechar"]');
     const modalProduto = document.querySelector('[data-modalProduto="modal"]');
@@ -121,7 +135,7 @@ function initModalProduto() {
 
     function removerItem() {
         let quantidadeDeItens = +spanItem.innerText;
-        if(quantidadeDeItens === 0) {
+        if (quantidadeDeItens === 0) {
 
         } else {
             spanItem.innerText = quantidadeDeItens - 1;
@@ -130,10 +144,10 @@ function initModalProduto() {
 
     eventos.forEach((evento) => {
         botaoFecharModal.addEventListener(evento, fechaModal);
-    }) 
+    })
 
-    imagensDosProdutos.forEach((i) => {i.addEventListener('click', abreModal);});
-    botaoDosProdutos.forEach((i) => {i.addEventListener('click', abreModal);});
+    imagensDosProdutos.forEach((i) => { i.addEventListener('click', abreModal); });
+    botaoDosProdutos.forEach((i) => { i.addEventListener('click', abreModal); });
 
     botaoAdicionarItem.addEventListener('click', adicionarItem);
     botaoRemoverItem.addEventListener('click', removerItem);
